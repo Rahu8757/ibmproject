8c0df1b3d490a0c70049775c3a4ea612c9f7277b
RAHUL KUMAR <RAHUL.Kumar13@ibm.com>
2021-04-03 14:44:48 +0530

    Add project files.

---
+++ march17/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="march17.Global" Language="C#" %>
---
+++ march17/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace march17
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            if (Membership.ValidateUser(Login1.UserName, Login1.Password))
+            {
+                FormsAuthentication.RedirectFromLoginPage(Login1.UserName, false);
+
+                if (Login1.RememberMeSet == true)
+                {
+                    FormsAuthentication.RedirectFromLoginPage(Login1.UserName, true);
+                }
+
+            }
+        }
+    }
+}
